9a2ab896ec221b9dbf12d5c0a3185c404581d1c7
Carol Holmstedt <cjkulzer@gmail.com>
2024-06-13 15:13:29 -0700

    Added budget alert based on funds remaining

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -8,16 +8,20 @@ const Budget = () => {
         return (total += item.cost);
     }, 0);
     const handleBudgetChange = (event) => {
+        let remaining = budget - totalExpenses;
         if (event.target.value < totalExpenses) {
             alert("Budget cannot be lower than amount spent so far");
-        } else {
+        } else if (event.target.value > remaining) {
+            alert("Budget cannot exceed remaining funds " + currency.symbol + remaining);
+        } 
+        else {
             setNewBudget(event.target.value);
         }
     }
     return (
         <div className='alert alert-secondary'>
             <span>Budget: {currency.symbol}{budget}</span>
-            <input type="number" step="10" max="20000" value={newBudget} onChange={handleBudgetChange}></input>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
         </div>
     );
 };
